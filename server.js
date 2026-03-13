167bde6d60ee71548b7c4122d4d60efbe58533ca
rohitdalvi18 <rohit.dalvi2001@gmail.com>
2026-03-12 19:32:41 -0700

    update script.js

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.post("/chat", (req, res) => {
 
 // 2.5 Listen on port 3000
 // changed the port for testing
-app.listen(3001, () => {
-  console.log("Server is running on http://localhost:3001");
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
